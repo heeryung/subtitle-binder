fe063434d5f1ca7c502d7f8395e830b6581c5e45
heeryung.c@gmail.com <heeryung.c@gmail.com>
2020-04-16 20:35:27 -0400

    timer added

--- submit.js
+++ submit.js
@@ -238,6 +238,7 @@ define([
             xhr.send(JSON.stringify(data));
         }
 
+
         var submitNotebookInfo = function () {
             // create a dialog modal and let students know their answer is saved
             oldSaveNotebook();
@@ -380,6 +381,81 @@ define([
 
 
         };
+
+
+
+
+        /////submitNotebookInfo_ver_for_timer.js
+        var submitNotebookInfo = function () {
+            // create a dialog modal and let students know their answer is saved
+            oldSaveNotebook();
+            var nbName = Jupyter.notebook.get_cell(0).metadata['name'];
+            var description = Jupyter.notebook.get_cell(0).metadata['description'];
+            var license = Jupyter.notebook.get_cell(0).metadata['license'];
+            var form = $("<form></form>").attr("id", "save-form");
+            form.append("<h4>Warning: Time is up. Your response has recorded. Please proceed. <br></h4>");
+
+            function encodeQueryData(data) {
+                let paramResult = encodeURIComponent('user') + '=' + encodeURIComponent(data['user']);
+                return paramResult;
+            }
+
+            function switchPage() {
+                // get the values from the form
+                const currentQueryString = window.location.search;
+                let urlParams = new URLSearchParams(currentQueryString); //This doesn't work on IE
+                let userid = urlParams.get('id');
+
+                const data = {'user': userid}
+
+                const queryString = encodeQueryData(data)
+
+                // let url = 'http://localhost:8000/'
+                let url = 'https://umich.qualtrics.com/jfe/form/SV_1KPMxz46OuSiPTT'
+
+                // build url
+                url += '?' + queryString
+                window.location = url
+            }
+
+
+
+            dialog.modal({
+                title: i18n.msg._('Submit Your Final Solution'),
+                body: form,
+                buttons: {
+                    'Submit': {
+                        'class': 'btn-primary', 'click': function () {
+                            handlerSubmit(license, nbName, description);
+                            dispatchSubmitSolutionEvent();
+                            Jupyter.notebook.get_cell(0).metadata.submit = "submit";
+                            oldSaveNotebook();
+//                             var difficulty = ratingsMap['difficulty-rating-star'];
+//                             var quality = ratingsMap['quality-rating-star'];
+//                             var comments = $("#comments").val();
+//                             submitRating(difficulty, quality, comments);
+                            $($('#save-notbook').children()[0]).prop('disabled', true);
+                            $('#submit-solution').attr('disabled', 'disabled');
+                            $('#submit-solution').hide();
+
+                            // For now, we are not using the show answer buttons.
+                            // We will keep them hidden, but programmatically click on
+                            // all of them to reveal all answers at once.
+                            // $('.part-answer-button').show();
+                            // $('.part-answer-button').click();
+                            $.when(getSolutions.insertSolutionCells()).then(switchPage());
+
+                        }
+                    }
+                },
+                notebook: Jupyter.notebook,
+                keyboard_manager: Jupyter.keyboard_manager
+            });
+
+
+        };
+
+        /////
 
         var umich_metadata = IPython.notebook.metadata.umich;
         var umich_metadata_submit = umich_metadata.submit;
---
+++ timer.js
@@ -0,0 +1,72 @@
+define([
+    'jquery',
+    'require',
+    'contents',
+    'base/js/namespace',
+    'base/js/utils',
+    'tree/js/newnotebook',
+    'base/js/i18n',
+    'base/js/dialog',
+    'base/js/events',
+    'services/config',
+    'nbextensions/url',
+    'nbextensions/username',
+    'nbextensions/control',
+    'nbextensions/submit'
+], function ($,
+             require,
+             contents_service,
+             IPython,
+             utils,
+             newnotebook,
+             i18n,
+             dialog,
+             events,
+             config,
+             getUrl,
+             getUsername,
+             control,
+             submitNotebookInfoTimer) {
+    function load_ipython_extension() {
+        function timer(){
+            var timenow = new Date();
+            var endtime = new Date(timenow.getTime() + 10*60000); //mins*60000
+            //timer function
+            var t = Date.parse(endtime) - Date.parse(new Date());
+            var seconds = Math.floor( (t/1000) % 60 );
+            var minutes = Math.floor( (t/1000/60) % 60 );
+
+            // Result is output to the specific element
+            document.getElementById("mins").innerHTML = minutes + "m "
+            document.getElementById("secs").innerHTML = seconds + "s "
+
+            if (t < 0){
+                submitNotebookInfoTimer
+            }
+        };
+
+
+
+        var umich_metadata = IPython.notebook.metadata.umich;
+        var umich_metadata_submit = umich_metadata.submit;
+
+        if (umich_metadata_submit === "yes") {
+
+            Jupyter.toolbar.add_buttons_group([{
+                label: 'Timer',
+                id: 'timerBar',
+                handler: timer
+            }]);
+        };
+    }
+
+     var nb_content = JSON.parse(JSON.stringify(Jupyter.notebook));
+     if (IPython.notebook.metadata.umich.submit === "yes") {
+         if (nb_content.cells[0].metadata.submit != "submit") {
+             return {
+                 load_ipython_extension: load_ipython_extension
+             };
+         }
+     }
+
+ });
